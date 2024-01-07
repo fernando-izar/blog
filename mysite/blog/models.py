52a52a754934b195d1abc00772c5f2d58ac994ee
fernando-izar <fizar@me.com>
2024-01-07 09:11:06 -0300

    feat: url blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status = 'published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -26,3 +33,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
