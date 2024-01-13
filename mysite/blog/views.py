3f9aa1b370d293e249cc8fd5f8ceb6f58ac3dcb0
fernando-izar <fizar@me.com>
2024-01-13 07:21:23 -0300

    feat: PostListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     posts = Post.published.all()
