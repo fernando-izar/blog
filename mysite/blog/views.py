9ff75ec802a548b0f8cfa345cc2e3d280fd415b3
fernando-izar <fizar@me.com>
2024-01-14 18:53:17 -0300

    feat: send_mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,10 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
+from decouple import config
+
+email = config('EMAIL_HOST_USER')
 
 
 class PostListView(ListView):
@@ -14,6 +18,7 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status = 'published')
+    sent = False
 
     if request.method == 'POST':
         # Form was submitted
@@ -22,9 +27,17 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                        f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, email, [cd['to']])
+            sent = True
+                      
     else: 
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post':post, 'form':form})
+    return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
 
 def post_list(request):
     posts = Post.published.all()
